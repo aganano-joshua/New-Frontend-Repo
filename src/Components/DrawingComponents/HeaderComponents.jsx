b0977735c020b63d023a3a25201f6fdf877d557c
Jay_Tech <agananoadeyemi001@gmail.com>
2024-11-02 01:52:51 +0100

    Still working on header

--- src/Components/DrawingComponents/HeaderComponents.jsx
+++ src/Components/DrawingComponents/HeaderComponents.jsx
@@ -34,8 +34,8 @@ const SketchDropDown = () => {
     );
     return (
         <>
-            <div className="shadow sm:w-[20rem] md:w-[25rem]">
-                <ul className="absolute right-0 top-0 bg-[#FCFFF7] md:w-[25rem] sm:w-[20rem] h-[30rem] font-bold pl-[20px] flex flex-col gap-[6px] shadow rounded-[10px]">
+            <div className="shadow w-[25rem] sm:w-[25rem] md:w-[25rem]">
+                <ul className="absolute right-0 top-0 bg-[#FCFFF7] w-[18rem] md:w-[25rem] sm:w-[20rem] h-[30rem] font-bold pl-[20px] flex flex-col gap-[6px] shadow rounded-[10px]">
                     <div className="flex flex-row gap-1 items-center mt-[20px] shadow-md mr-[1.5rem] pl-[10px] rounded-[40px]">
                         <IoSearch size={30}/>
                         <input type="search" placeholder="Search" value={searchQuery} onChange={handleSearch} className="bg-transparent outline-none h-[2.5rem] pl-[10px] w-[100%] pr-[10px] border-none"/>
@@ -78,7 +78,7 @@ const DropDownMenu = () => {
       };
     return (
         <>
-            <div className="shadow">
+            <div className="shadow" style={{backgroundImage: `url(${assets.brush})`, width: "100%"}}>
                 <ul className="absolute right-[20px] top-[70px] bg-[#FCFFF7] w-[10rem] h-[10rem] font-bold pl-[20px] flex flex-col gap-[6px] shadow rounded-[10px] pt-[3px] cursor-pointer">
                     <li>Daily Challenge</li>
                     <li onClick={toggleVisibility}>Sketch</li>
